4cb7db21de2c864f18754cf99fdac4df1c7ed78e
Mike Nolan <mikechristopher@gmail.com>
2025-10-14 12:16:34 +0100

    mobile update

--- assets/script.js
+++ assets/script.js
@@ -100,3 +100,10 @@ function initTechnology(technology) {
 
   showTech(0); // Default to first technology
 }
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  menu.classList.toggle("show");
+});
